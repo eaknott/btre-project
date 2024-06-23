606f801ed2e41a87e229168857568b828ee15af0
eaknott <emknotty@gmail.com>
2024-06-23 13:27:47 +0800

    Make migrations for listings and realtors tables in db

--- btre/settings.py
+++ btre/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 

b5d5cb9f71f36125125f542ec1c2eb979deb16dc
eaknott <emknotty@gmail.com>
2024-06-23 17:52:49 +0800

    Add branding and custom styling to Admin area and add RealtorAdmin and ListingAdmin classes to ModelAdmin

--- btre/settings.py
+++ btre/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/Adstatic/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]

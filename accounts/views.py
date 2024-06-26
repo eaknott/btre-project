d8b2a172b009ba4d9396c301f1e50a982155ec73
eaknott <emknotty@gmail.com>
2024-06-26 21:01:57 +0800

    Add authentication functionality to login def

--- accounts/views.py
+++ accounts/views.py
@@ -43,8 +43,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login user
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate[username=username, password=password]
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect(request, 'login')
     else:
         return render(request, 'accounts/login.html')
 

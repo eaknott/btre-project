ca1f4b8c80b76efb4db5b684644128321b4beb10
eaknott <emknotty@gmail.com>
2024-07-01 11:38:55 +0800

    Add local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -140,3 +140,8 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
